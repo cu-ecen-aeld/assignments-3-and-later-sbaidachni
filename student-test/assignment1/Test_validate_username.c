17e14a3013f5eb7d7177882215986cdac6993e59
sbaidachni <sbaydach@microsoft.com>
2023-12-15 15:44:31 -1000

    Update Test_validate_username.c

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,5 +18,7 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    char *user_name = my_username();
+    char *config_user_name = malloc_username_from_conf_file();
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(user_name, config_user_name);
 }
