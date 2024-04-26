ee614b013e7725736d5008b76311d0b05e466015
sbaidachni <sbaidachni@gmail.com>
2024-04-25 20:29:03 -0700

    4.2 fix

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -57,7 +57,7 @@ do
 	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
 
 echo "${OUTPUTSTRING}" > /tmp/assignment4-result.txt
 
